413fbead5633a88e5cd620010256b01cf74920df
damianwalendzik <damianwalendzik7@gmail.com>
2024-01-24 20:35:55 +0100

    button functionality added, starting to move to game mechanics

--- cell.py
+++ cell.py
@@ -1,22 +1,65 @@
 from tkinter import Button
+from random import sample
+import settings
 class Cell:
-    def __init__(self, is_mine=False):
+    all = []
+    def __init__(self,x ,y, is_mine=False):
         self.is_mine=is_mine
         self.cell_btn_object = None
+        self.x=x
+        self.y=y
+        #Append the obj to the Cell.all list
+        Cell.all.append(self)
 
     def create_btn_object(self, location):
         btn = Button(
             location,
-            text='Text'
+            width=12,
+            height=3
         )
         btn.bind('<Button-1>', self.left_click_actions) #<Button-1> means left click
         btn.bind('<Button-3>', self.right_click_actions) #<Button-3> means right click
         self.cell_btn_object = btn
         
     def left_click_actions(self, event):
-        print(event)
-        print("I am left clicked")
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
+
+    def get_cell_by_axis(self, x, y):
+        #Return a cell obj based on the value of x,y
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+            
+    def show_mine(cell):
+        cell.cell_btn_object.configure(bg='red')
+
+    def show_cell(self):
+        surrounding_cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1)
+        ]
+        print(surrounding_cells)
 
     def right_click_actions(self, event):
         print(event)
         print("I am right clicked")
+
+    @staticmethod
+    def randomize_mines():
+        picked_cells = sample(
+            Cell.all, settings.MINES_COUNT
+        )
+        for picked_cell in picked_cells:
+            picked_cell.is_mine = True
+
+    def __repr__(self):
+        return f"Cell({self.x}, {self.y})"
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ root.resizable(False, False)
 
 top_frame = Frame(
     root,
-    bg='red', #change later
+    bg='black', #change later
     width=utils.width_percentage(100),
     height=utils.height_percentage(20)
     )
@@ -19,7 +19,7 @@ top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
-    bg='blue',
+    bg='black',
     width=utils.width_percentage(25),
     height=utils.height_percentage(80)
 )
@@ -27,7 +27,7 @@ left_frame.place(x=0,y=utils.height_percentage(20))
 
 center_frame = Frame(
     root,
-    bg='green',
+    bg='black',
     width=utils.width_percentage(75),
     height=utils.height_percentage(80)
 )
@@ -37,9 +37,11 @@ center_frame.place(x=utils.width_percentage(25),y=utils.height_percentage(20))
 
 for x in range(settings.GRID_SIZE):
     for y in range(settings.GRID_SIZE):
-        c = Cell()
+        c = Cell(x, y)
         c.create_btn_object(center_frame)
         c.cell_btn_object.grid(
             column=x, row=y
         )
+
+Cell.randomize_mines()
 root.mainloop()
