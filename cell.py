4cd03aca4b5aecfdfa9c5b65204451684e05131a
damianwalendzik <damianwalendzik7@gmail.com>
2024-01-24 20:59:58 +0100

    logic more or less finished

--- cell.py
+++ cell.py
@@ -35,9 +35,9 @@ class Cell:
             
     def show_mine(cell):
         cell.cell_btn_object.configure(bg='red')
-
-    def show_cell(self):
-        surrounding_cells = [
+    
+    def surrounded_cells(self):
+        cells = [
             self.get_cell_by_axis(self.x - 1, self.y - 1),
             self.get_cell_by_axis(self.x - 1, self.y),
             self.get_cell_by_axis(self.x - 1, self.y + 1),
@@ -47,7 +47,17 @@ class Cell:
             self.get_cell_by_axis(self.x + 1, self.y + 1),
             self.get_cell_by_axis(self.x, self.y + 1)
         ]
-        print(surrounding_cells)
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+
+
+    @property
+    def surrounded_cells_mines_length(self):
+        counter = 0
+        for cell in self.surrounded_cells():
+            if cell.is_mine:
+                counter+=1
+        return counter
 
     def right_click_actions(self, event):
         print(event)
@@ -60,6 +70,9 @@ class Cell:
         )
         for picked_cell in picked_cells:
             picked_cell.is_mine = True
+    
+    def show_cell(self):
+        self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
 
     def __repr__(self):
         return f"Cell({self.x}, {self.y})"
